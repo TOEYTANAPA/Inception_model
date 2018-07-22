710766afc0de745fea3b6d047d07b249c2cbfa5e
Earnn <earnlune@gmail.com>
2018-07-23 01:03:52 +0700

    update

--- earn/test.py
+++ earn/test.py
@@ -83,222 +83,218 @@ reduce1x1 = 16
 dropout=0.5
 graph = tf.Graph()
 
-if FLAGS.job_name == "ps":
-    server.join()
-elif FLAGS.job_name == "worker":
-    with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d" % FLAGS.task_index,cluster=cluster)):global_step = tf.get_variable('global_step', [],initializer=tf.constant_initializer(0),trainable=False)
-        # with graph.as_default():
-        #train data and labels
-        X = tf.placeholder(tf.float32,shape=(batch_size,28,28,1))
-        y_ = tf.placeholder(tf.float32,shape=(batch_size,10))
+with graph.as_default():
+            #train data and labels
+            X = tf.placeholder(tf.float32,shape=(batch_size,28,28,1))
+            y_ = tf.placeholder(tf.float32,shape=(batch_size,10))
 
-        #validation data
-        tf_valX = tf.placeholder(tf.float32,shape=(len(valX),28,28,1))
+            #validation data
+            tf_valX = tf.placeholder(tf.float32,shape=(len(valX),28,28,1))
 
-        #test data
-        tf_testX=tf.placeholder(tf.float32,shape=(test_batch_size,28,28,1))
+            #test data
+            tf_testX=tf.placeholder(tf.float32,shape=(test_batch_size,28,28,1))
 
 
-        def createWeight(size,Name):
-            return tf.Variable(tf.truncated_normal(size, stddev=0.1),
-                              name=Name)
-
-
-        start_time = time.time()
-        def createBias(size,Name):
-            return tf.Variable(tf.constant(0.1,shape=size),
-                              name=Name)
-
-
-
-        def conv2d_s1(x,W):
-            return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
-
-
-        def max_pool_3x3_s1(x):
-            return tf.nn.max_pool(x,ksize=[1,3,3,1],
-                                 strides=[1,1,1,1],padding='SAME')
-
-        #Inception Module1
-        #
-        #follows input
-        W_conv1_1x1_1 = createWeight([1,1,1,map1],'W_conv1_1x1_1')
-        b_conv1_1x1_1 = createWeight([map1],'b_conv1_1x1_1')
-
-        #follows input
-        W_conv1_1x1_2 = createWeight([1,1,1,reduce1x1],'W_conv1_1x1_2')
-        b_conv1_1x1_2 = createWeight([reduce1x1],'b_conv1_1x1_2')
-
-        #follows input
-        W_conv1_1x1_3 = createWeight([1,1,1,reduce1x1],'W_conv1_1x1_3')
-        b_conv1_1x1_3 = createWeight([reduce1x1],'b_conv1_1x1_3')
-
-        #follows 1x1_2
-        W_conv1_3x3 = createWeight([3,3,reduce1x1,map1],'W_conv1_3x3')
-        b_conv1_3x3 = createWeight([map1],'b_conv1_3x3')
-
-        #follows 1x1_3
-        W_conv1_5x5 = createWeight([5,5,reduce1x1,map1],'W_conv1_5x5')
-        b_conv1_5x5 = createBias([map1],'b_conv1_5x5')
-
-        #follows max pooling
-        W_conv1_1x1_4= createWeight([1,1,1,map1],'W_conv1_1x1_4')
-        b_conv1_1x1_4= createWeight([map1],'b_conv1_1x1_4')
-
-
-
-        #Inception Module2
-        #
-        #follows inception1
-        W_conv2_1x1_1 = createWeight([1,1,4*map1,map2],'W_conv2_1x1_1')
-        b_conv2_1x1_1 = createWeight([map2],'b_conv2_1x1_1')
-
-        #follows inception1
-        W_conv2_1x1_2 = createWeight([1,1,4*map1,reduce1x1],'W_conv2_1x1_2')
-        b_conv2_1x1_2 = createWeight([reduce1x1],'b_conv2_1x1_2')
-
-        #follows inception1
-        W_conv2_1x1_3 = createWeight([1,1,4*map1,reduce1x1],'W_conv2_1x1_3')
-        b_conv2_1x1_3 = createWeight([reduce1x1],'b_conv2_1x1_3')
-
-        #follows 1x1_2
-        W_conv2_3x3 = createWeight([3,3,reduce1x1,map2],'W_conv2_3x3')
-        b_conv2_3x3 = createWeight([map2],'b_conv2_3x3')
-
-        #follows 1x1_3
-        W_conv2_5x5 = createWeight([5,5,reduce1x1,map2],'W_conv2_5x5')
-        b_conv2_5x5 = createBias([map2],'b_conv2_5x5')
-
-        #follows max pooling
-        W_conv2_1x1_4= createWeight([1,1,4*map1,map2],'W_conv2_1x1_4')
-        b_conv2_1x1_4= createWeight([map2],'b_conv2_1x1_4')
-
-
-
-        #Fully connected layers
-        #since padding is same, the feature map with there will be 4 28*28*map2
-        W_fc1 = createWeight([28*28*(4*map2),num_fc1],'W_fc1')
-        b_fc1 = createBias([num_fc1],'b_fc1')
-
-        W_fc2 = createWeight([num_fc1,num_fc2],'W_fc2')
-        b_fc2 = createBias([num_fc2],'b_fc2')
-
-        def model(x,train=True):
-            #Inception Module 1
-
-            start_time = time.time()
-            conv1_1x1_1 = conv2d_s1(x,W_conv1_1x1_1)+b_conv1_1x1_1
-            print("--- conv1_1x1_1:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            def createWeight(size,Name):
+                return tf.Variable(tf.truncated_normal(size, stddev=0.1),
+                                  name=Name)
 
 
             start_time = time.time()
-            conv1_1x1_2 = tf.nn.relu(conv2d_s1(x,W_conv1_1x1_2)+b_conv1_1x1_2)
-            print("--- conv1_1x1_2:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            def createBias(size,Name):
+                return tf.Variable(tf.constant(0.1,shape=size),
+                                  name=Name)
 
 
-            start_time = time.time()
-            conv1_1x1_3 = tf.nn.relu(conv2d_s1(x,W_conv1_1x1_3)+b_conv1_1x1_3)
-            print("--- conv1_1x1_3:  %s seconds ---" % (time.time() - start_time))
-            print(line)
 
-            start_time = time.time()
-            conv1_3x3 = conv2d_s1(conv1_1x1_2,W_conv1_3x3)+b_conv1_3x3
-            print("--- conv1_3x3:  %s seconds ---" % (time.time() - start_time))
-            print(line)
-
-            start_time = time.time()
-            conv1_5x5 = conv2d_s1(conv1_1x1_3,W_conv1_5x5)+b_conv1_5x5
-            print("--- conv1_5x5:  %s seconds ---" % (time.time() - start_time))
-            print(line)
-
-            start_time = time.time()
-            maxpool1 = max_pool_3x3_s1(x)
-            print("--- maxpool1:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            def conv2d_s1(x,W):
+                return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
 
 
-            start_time = time.time()
-            conv1_1x1_4 = conv2d_s1(maxpool1,W_conv1_1x1_4)+b_conv1_1x1_4
-            print("--- conv1_1x1_4:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            def max_pool_3x3_s1(x):
+                return tf.nn.max_pool(x,ksize=[1,3,3,1],
+                                     strides=[1,1,1,1],padding='SAME')
 
-            #concatenate all the feature maps and hit them with a relu
-            inception1 = tf.nn.relu(tf.concat([conv1_1x1_1,conv1_3x3,conv1_5x5,conv1_1x1_4],3))
+            #Inception Module1
+            #
+            #follows input
+            W_conv1_1x1_1 = createWeight([1,1,1,map1],'W_conv1_1x1_1')
+            b_conv1_1x1_1 = createWeight([map1],'b_conv1_1x1_1')
 
-            #Inception Module 2
-            start_time = time.time()
-            conv2_1x1_1 = conv2d_s1(inception1,W_conv2_1x1_1)+b_conv2_1x1_1
-            print("--- conv2_1x1_1:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            #follows input
+            W_conv1_1x1_2 = createWeight([1,1,1,reduce1x1],'W_conv1_1x1_2')
+            b_conv1_1x1_2 = createWeight([reduce1x1],'b_conv1_1x1_2')
 
+            #follows input
+            W_conv1_1x1_3 = createWeight([1,1,1,reduce1x1],'W_conv1_1x1_3')
+            b_conv1_1x1_3 = createWeight([reduce1x1],'b_conv1_1x1_3')
 
-            start_time = time.time()
-            conv2_1x1_2 = tf.nn.relu(conv2d_s1(inception1,W_conv2_1x1_2)+b_conv2_1x1_2)
-            print("--- conv2_1x1_2:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            #follows 1x1_2
+            W_conv1_3x3 = createWeight([3,3,reduce1x1,map1],'W_conv1_3x3')
+            b_conv1_3x3 = createWeight([map1],'b_conv1_3x3')
 
-            start_time = time.time()
-            conv2_1x1_3 = tf.nn.relu(conv2d_s1(inception1,W_conv2_1x1_3)+b_conv2_1x1_3)
-            print("--- conv2_1x1_3:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            #follows 1x1_3
+            W_conv1_5x5 = createWeight([5,5,reduce1x1,map1],'W_conv1_5x5')
+            b_conv1_5x5 = createBias([map1],'b_conv1_5x5')
 
-            start_time = time.time()
-            conv2_3x3 = conv2d_s1(conv2_1x1_2,W_conv2_3x3)+b_conv2_3x3
-            print("--- conv2_3x3:  %s seconds ---" % (time.time() - start_time))
-            print(line)
-
-            start_time = time.time()
-            conv2_5x5 = conv2d_s1(conv2_1x1_3,W_conv2_5x5)+b_conv2_5x5
-            print("--- conv2_5x5:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            #follows max pooling
+            W_conv1_1x1_4= createWeight([1,1,1,map1],'W_conv1_1x1_4')
+            b_conv1_1x1_4= createWeight([map1],'b_conv1_1x1_4')
 
 
-            start_time = time.time()
-            maxpool2 = max_pool_3x3_s1(inception1)
-            print("--- maxpool2:  %s seconds ---" % (time.time() - start_time))
-            print(line)
 
-            start_time = time.time()
-            conv2_1x1_4 = conv2d_s1(maxpool2,W_conv2_1x1_4)+b_conv2_1x1_4
-            print("--- conv2_1x1_4:  %s seconds ---" % (time.time() - start_time))
-            print(line)
+            #Inception Module2
+            #
+            #follows inception1
+            W_conv2_1x1_1 = createWeight([1,1,4*map1,map2],'W_conv2_1x1_1')
+            b_conv2_1x1_1 = createWeight([map2],'b_conv2_1x1_1')
+
+            #follows inception1
+            W_conv2_1x1_2 = createWeight([1,1,4*map1,reduce1x1],'W_conv2_1x1_2')
+            b_conv2_1x1_2 = createWeight([reduce1x1],'b_conv2_1x1_2')
+
+            #follows inception1
+            W_conv2_1x1_3 = createWeight([1,1,4*map1,reduce1x1],'W_conv2_1x1_3')
+            b_conv2_1x1_3 = createWeight([reduce1x1],'b_conv2_1x1_3')
+
+            #follows 1x1_2
+            W_conv2_3x3 = createWeight([3,3,reduce1x1,map2],'W_conv2_3x3')
+            b_conv2_3x3 = createWeight([map2],'b_conv2_3x3')
+
+            #follows 1x1_3
+            W_conv2_5x5 = createWeight([5,5,reduce1x1,map2],'W_conv2_5x5')
+            b_conv2_5x5 = createBias([map2],'b_conv2_5x5')
+
+            #follows max pooling
+            W_conv2_1x1_4= createWeight([1,1,4*map1,map2],'W_conv2_1x1_4')
+            b_conv2_1x1_4= createWeight([map2],'b_conv2_1x1_4')
 
 
-            #concatenate all the feature maps and hit them with a relu
-            start_time = time.time()
-            inception2 = tf.nn.relu(tf.concat([conv2_1x1_1,conv2_3x3,conv2_5x5,conv2_1x1_4],3))
-            print("--- inception2:  %s seconds ---" % (time.time() - start_time))
-            print(line)
-
-            #flatten features for fully connected layer
-            start_time = time.time()
-            inception2_flat = tf.reshape(inception2,[-1,28*28*4*map2])
-            print("--- inception2_flat:  %s seconds ---" % (time.time() - start_time))
-            print(line)
 
             #Fully connected layers
-            if train:
-                h_fc1 =tf.nn.dropout(tf.nn.relu(tf.matmul(inception2_flat,W_fc1)+b_fc1),dropout)
-            else:
-                h_fc1 = tf.nn.relu(tf.matmul(inception2_flat,W_fc1)+b_fc1)
+            #since padding is same, the feature map with there will be 4 28*28*map2
+            W_fc1 = createWeight([28*28*(4*map2),num_fc1],'W_fc1')
+            b_fc1 = createBias([num_fc1],'b_fc1')
 
-            return tf.matmul(h_fc1,W_fc2)+b_fc2
+            W_fc2 = createWeight([num_fc1,num_fc2],'W_fc2')
+            b_fc2 = createBias([num_fc2],'b_fc2')
 
-    #     tf.nn.softmax_cross_entropy_with_logits(logits = yPredbyNN, labels=Y)
-        loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(logits=model(X),labels=y_))
-        opt = tf.train.AdamOptimizer(1e-4).minimize(loss)
+            def model(x,train=True):
+                #Inception Module 1
 
-        predictions_val = tf.nn.softmax(model(tf_valX,train=False))
-        predictions_test = tf.nn.softmax(model(tf_testX,train=False))
+                start_time = time.time()
+                conv1_1x1_1 = conv2d_s1(x,W_conv1_1x1_1)+b_conv1_1x1_1
+                print("--- conv1_1x1_1:  %s seconds ---" % (time.time() - start_time))
+                print(line)
 
-        #initialize variable
-        init = tf.initialize_all_variables()
 
-        #use to save variables so we can pick up later
-        saver = tf.train.Saver()
+                start_time = time.time()
+                conv1_1x1_2 = tf.nn.relu(conv2d_s1(x,W_conv1_1x1_2)+b_conv1_1x1_2)
+                print("--- conv1_1x1_2:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+
+                start_time = time.time()
+                conv1_1x1_3 = tf.nn.relu(conv2d_s1(x,W_conv1_1x1_3)+b_conv1_1x1_3)
+                print("--- conv1_1x1_3:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                start_time = time.time()
+                conv1_3x3 = conv2d_s1(conv1_1x1_2,W_conv1_3x3)+b_conv1_3x3
+                print("--- conv1_3x3:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                start_time = time.time()
+                conv1_5x5 = conv2d_s1(conv1_1x1_3,W_conv1_5x5)+b_conv1_5x5
+                print("--- conv1_5x5:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                start_time = time.time()
+                maxpool1 = max_pool_3x3_s1(x)
+                print("--- maxpool1:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+
+                start_time = time.time()
+                conv1_1x1_4 = conv2d_s1(maxpool1,W_conv1_1x1_4)+b_conv1_1x1_4
+                print("--- conv1_1x1_4:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                #concatenate all the feature maps and hit them with a relu
+                inception1 = tf.nn.relu(tf.concat([conv1_1x1_1,conv1_3x3,conv1_5x5,conv1_1x1_4],3))
+
+                #Inception Module 2
+                start_time = time.time()
+                conv2_1x1_1 = conv2d_s1(inception1,W_conv2_1x1_1)+b_conv2_1x1_1
+                print("--- conv2_1x1_1:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+
+                start_time = time.time()
+                conv2_1x1_2 = tf.nn.relu(conv2d_s1(inception1,W_conv2_1x1_2)+b_conv2_1x1_2)
+                print("--- conv2_1x1_2:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                start_time = time.time()
+                conv2_1x1_3 = tf.nn.relu(conv2d_s1(inception1,W_conv2_1x1_3)+b_conv2_1x1_3)
+                print("--- conv2_1x1_3:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                start_time = time.time()
+                conv2_3x3 = conv2d_s1(conv2_1x1_2,W_conv2_3x3)+b_conv2_3x3
+                print("--- conv2_3x3:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                start_time = time.time()
+                conv2_5x5 = conv2d_s1(conv2_1x1_3,W_conv2_5x5)+b_conv2_5x5
+                print("--- conv2_5x5:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+
+                start_time = time.time()
+                maxpool2 = max_pool_3x3_s1(inception1)
+                print("--- maxpool2:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                start_time = time.time()
+                conv2_1x1_4 = conv2d_s1(maxpool2,W_conv2_1x1_4)+b_conv2_1x1_4
+                print("--- conv2_1x1_4:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+
+                #concatenate all the feature maps and hit them with a relu
+                start_time = time.time()
+                inception2 = tf.nn.relu(tf.concat([conv2_1x1_1,conv2_3x3,conv2_5x5,conv2_1x1_4],3))
+                print("--- inception2:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                #flatten features for fully connected layer
+                start_time = time.time()
+                inception2_flat = tf.reshape(inception2,[-1,28*28*4*map2])
+                print("--- inception2_flat:  %s seconds ---" % (time.time() - start_time))
+                print(line)
+
+                #Fully connected layers
+                if train:
+                    h_fc1 =tf.nn.dropout(tf.nn.relu(tf.matmul(inception2_flat,W_fc1)+b_fc1),dropout)
+                else:
+                    h_fc1 = tf.nn.relu(tf.matmul(inception2_flat,W_fc1)+b_fc1)
+
+                return tf.matmul(h_fc1,W_fc2)+b_fc2
+
+        #     tf.nn.softmax_cross_entropy_with_logits(logits = yPredbyNN, labels=Y)
+            loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(logits=model(X),labels=y_))
+            opt = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+            predictions_val = tf.nn.softmax(model(tf_valX,train=False))
+            predictions_test = tf.nn.softmax(model(tf_testX,train=False))
+
+            #initialize variable
+            init = tf.initialize_all_variables()
+
+            #use to save variables so we can pick up later
+            saver = tf.train.Saver()
 
 
     #set use_previous=1 to use file_path model
