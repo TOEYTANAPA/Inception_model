05d205335073a49fb3a20258694cbeaf72d8e333
Earnn <earnlune@gmail.com>
2018-07-27 21:53:55 +0700

    a

--- earn/baseline.py
+++ earn/baseline.py
@@ -336,14 +336,14 @@ with tf.device('/device:GPU:1'):
             if s%100 == 0:
                 feed_dict = {tf_valX : valX}
                 preds=sess.run(predictions_val,feed_dict=feed_dict)
-
+                acc = accuracy(val_lb,preds)
                 print ("step: "+str(s))
-                print ("validation accuracy: "+str(accuracy(val_lb,preds)))
+                print ("validation accuracy: "+str(acc))
                 print (" ")
                 print("--- %s seconds ---" % (time.time() - start_time))
                 print(line)
 
-                temp_acc = math.ceil(accuracy(val_lb,preds))
+                temp_acc = math.ceil(acc)
                 if accuracy < temp_acc or accuracy > temp_acc :
                     accuracy = temp_acc
                     convergence_time = time.time() - start_time
