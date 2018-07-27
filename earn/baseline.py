e014a18530170af2427243946a8c72f9ff02db3d
Earnn <earnlune@gmail.com>
2018-07-27 21:29:32 +0700

    baseline convergence time

--- earn/baseline.py
+++ earn/baseline.py
@@ -284,13 +284,16 @@ with graph.as_default():
 
 
 
-
+num_steps = 10000
+convergence_time = 0
+accuracy = 0
+step = 0
 
 #set use_previous=1 to use file_path model
 #set use_previous=0 to start model from scratch
 use_previous = 0
 with tf.device('/device:GPU:1'):
-    num_steps = 20000
+    num_steps = 10000
 
     #config=tf.ConfigProto(log_device_placement=True)
     # maximun alloc gpu 10% of MEM
@@ -338,6 +341,10 @@ with tf.device('/device:GPU:1'):
                 print("--- %s seconds ---" % (time.time() - start_time))
                 print(line)
 
+                temp_acc = int(accuracy(val_lb,preds))
+                if accuracy != temp_acc:
+                    convergence_time = time.time() - start_time
+                    step = s
             #get test accuracy and save model
             if s == (num_steps-1):
                 #create an array to store the outputs for the test
@@ -359,7 +366,10 @@ with tf.device('/device:GPU:1'):
                 # save_path = saver.save(sess,file_path)
                 # print("Model saved.")
         print("--- total_time %s seconds ---"% (time.time() - total_time))  
-    #sess.close()       
+        print("Convergence time: ",convergence_time)
+        print("Step: ",step)
+        print("--- total_time %s second ---"% (time.time() - start_time2))
+    sess.close()       
 
 
 
